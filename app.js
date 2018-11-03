c7125e44df6cec2bb4778384311aca8ee5432430
Sayho <sehoakasayho@naver.com>
2018-11-04 00:22:22 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+const app = express();
+var fs = require('fs');
+var path = require('path');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, function() {
+    console.log('Server is running on port ' + port);
+});
+
+app.get('/', function(req, res) {
+    fs.readFile('index.html', function(err, data) {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            res.writeHead(200, {'Content-Type' : 'text/html'});
+            res.end(data);
+        }
+    });
+});
